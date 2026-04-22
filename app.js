e978cd2e69da84f6aace20d3b8c900e21d4e296a
cheater314 <iphoneakk314@gmail.com>
2026-04-22 19:38:14 +0400

    refactor

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+import http from 'http';
+
+const { PORT } = process.env;
 
 
+const server = http.createServer((req, res) => {
+    const parsed = url.parse(req.url, true);
+    const pathname = parsed.pathname;
+    const query = parsed.query;
+
+    res.setHeader('Content-Type', 'application/json');
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
