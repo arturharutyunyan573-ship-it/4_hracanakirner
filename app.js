0ac6165991c323d9bcbfcfc5a32da8c701389643
Artur Harutyunyan <arturharutyunyan573@gmail.com>
2026-04-22 20:19:26 +0400

    new hendler1

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`app listning on port ${PORT}`);
 });
+const hendleer1 = (req, res, next) => {
+    console.log('hendleer1');
+    req.customData = 'This is a custom data from hendleer 1';
+    next();
+}
 
 const handler2 = (req, res) => {
     console.log('handler2');
     res.send(`handler2 received: ${req.customData}`);
 }
 
-app.get("/", handler2);
+app.get("/", handler2, hendleer1);
