c669cf741fffe6dfb06297ba49654dcabc15bd70
cheater314 <iphoneakk314@gmail.com>
2026-04-29 19:31:20 +0400

    add code

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 import HttpErrors from "http-errors";
+import * as fs from "node:fs";
 
 export default {
     async profile(req, res, next) {
@@ -25,4 +26,19 @@ export default {
             next(e);
         }
     },
+}
+
+
+
+export async function writeJSon(data) {
+    try{
+        await fs.writeFile(authorsFile, JSON.stringify(data, null, 2));
+    }catch (err) {
+        console.error(err);
+    }
+}
+
+export async function findById(id) {
+    const users = readJSON();
+    return users.find(user => user.id === id) || users;
 }
